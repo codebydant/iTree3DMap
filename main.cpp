441b0795c072924033aa685e0eef1c2aff7bb88e
Daniel <35694200+danielTobon43@users.noreply.github.com>
2018-07-23 10:04:55 -0500

    Add scale factor function

--- main.cpp
+++ main.cpp
@@ -15,6 +15,9 @@ void subscriberCallback(const cv::Mat& img){
 int main(int argc, char **argv){
 
   Utilities::help();
+  float scale;
+  Utilities::getScaleFactor(scale);
+
 
   /*************************
   STEP 1: ROS INTERFACE
@@ -31,25 +34,33 @@ int main(int argc, char **argv){
   STEP 2: 3D MAPPING
   **************************/
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_3dMap (new pcl::PointCloud<pcl::PointXYZRGB>());
-  //pcl::io::loadPCDFile("binary.pcd",*cloud_3dMap);
+
+
+  pcl::io::loadPCDFile("binary.pcd",*cloud_3dMap);
  // pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_3dMap_xyz (new pcl::PointCloud<pcl::PointXYZ>());
  // pcl::copyPointCloud(*cloud_3dMap,*cloud_3dMap_xyz);
 
-  Utilities::run_openMVG();
-  Utilities::createPMVS_Files();
-  Utilities::densifyWithPMVS(cloud_3dMap);
+  //Utilities::run_openMVG();
+ // Utilities::createPMVS_Files();
+ // Utilities::densifyWithPMVS(cloud_3dMap);
 
   /*************************
   STEP 3: SEGMENTATION
   **************************/  
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr tree_segmented (new pcl::PointCloud<pcl::PointXYZRGB>());
+  auto start = std::chrono::high_resolution_clock::now();
   Segmentation::extractTree(cloud_3dMap,tree_segmented);
+  auto end = std::chrono::high_resolution_clock::now();
+  auto difference = std::chrono::duration_cast<std::chrono::seconds>(end - start).count();
+  if(difference >=60){
+    std::cout << "Segmentation Time: " << difference/60 << " minutes" << std::endl;
+  }else{
+    std::cout << "Segmentation Time: " << difference << " seconds" << std::endl;
+  }
 
   /*************************
   STEP 4: UNIFORM SCALING
-  **************************/
-
-  float scale = 2;
+  **************************/  
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_3dMap_scaled (new pcl::PointCloud<pcl::PointXYZRGB>());
   Utilities::uniformScaling(tree_segmented,cloud_3dMap_scaled,scale,true);
 
