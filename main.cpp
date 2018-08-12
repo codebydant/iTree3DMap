e94ad39614122fb9e9b4f795409cd5586e8c7235
Daniel <35694200+danielTobon43@users.noreply.github.com>
2018-08-12 10:36:24 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -69,8 +69,10 @@ int main(int argc, char **argv){
   STEP 6: SEGMENTATION
   **************************/  
   pcl::PointCloud<pcl::PointXYZ>::Ptr trunk_segmented (new pcl::PointCloud<pcl::PointXYZ>());
+  pcl::PointCloud<pcl::PointXYZ>::Ptr tree_segmented (new pcl::PointCloud<pcl::PointXYZ>());
+  pcl::PointCloud<pcl::PointXYZ>::Ptr crown_segmented (new pcl::PointCloud<pcl::PointXYZ>());
   start = std::chrono::high_resolution_clock::now();
-  Segmentation::extractTree(cloud_3dMap_scaled,output_dir,trunk_segmented);
+  Segmentation::extractTree(cloud_3dMap_scaled,output_dir,trunk_segmented,tree_segmented,crown_segmented);
   end = std::chrono::high_resolution_clock::now();
   difference = std::chrono::duration_cast<std::chrono::seconds>(end - start).count();
   std::cout << "Trunk segmentation time: " << difference << " seconds" << std::endl;
@@ -78,7 +80,8 @@ int main(int argc, char **argv){
   /*************************
   STEP 7: DENDROMETRY MEASUREMENTS
   **************************/
-  //Dendrometry::estimate(cloud_3dMap_scaled);
+  pcl::PointXYZ minDBH,maxDBH;
+  Dendrometry::estimate(trunk_segmented,crown_segmented,minDBH,maxDBH);
 
   /*************************
   PCL VISUALIZER
@@ -100,37 +103,43 @@ int main(int argc, char **argv){
 
   int PORT2 = 0;
   viewer->createViewPort(0.33, 0.5, 0.66, 1.0, PORT2);
-  viewer->setBackgroundColor (0.3, 0.3, 0.3, PORT2);
+  viewer->setBackgroundColor (0, 0, 0, PORT2);
   viewer->addText("DENSE MAPPING", 10, 10, "PORT2", PORT2);
 
   int PORT3 = 0;
   viewer->createViewPort(0.66, 0.5, 1.0, 1.0, PORT3);
-  viewer->setBackgroundColor (0.2, 0.2, 0.2, PORT3);
-  viewer->addText("SCALE MAPPING", 10, 10, "PORT3", PORT3);
+  viewer->setBackgroundColor (0, 0, 0, PORT3);
+  //viewer->addLine(ptt1,pt2,0,255,0 ,"lenght",PORT3);
+  viewer->addText("CROWN", 10, 10, "PORT3", PORT3);
 
   int PORT4 = 0;
   viewer->createViewPort(0.0, 0.0, 0.33, 0.5, PORT4);
-  viewer->setBackgroundColor (0.1, 0.1, 0.1, PORT4);
-  viewer->addText("TRUNK AND CROWN", 10, 10, "PORT4", PORT4);
+  viewer->setBackgroundColor (0, 0, 0, PORT4);
+  viewer->addText("SCALE MAPPING", 10, 10, "PORT4", PORT4);
 
   int PORT5 = 0;
   viewer->createViewPort(0.33, 0.0, 0.66, 0.5, PORT5);
-  viewer->setBackgroundColor (0.1, 0.1, 0.1, PORT5);
-  viewer->addText("TRUNK", 10, 10, "PORT5", PORT5);
+  viewer->setBackgroundColor (0, 0, 0, PORT5);
+  viewer->addText("TREE SEGMENTED", 10, 10, "PORT5", PORT5);
 
   int PORT6 = 0;
   viewer->createViewPort(0.66, 0.0, 1.0, 0.5, PORT6);
-  viewer->setBackgroundColor (0.1, 0.1, 0.1, PORT6);
-  viewer->addText("CROWN", 10, 10, "PORT6", PORT6);
+  viewer->setBackgroundColor (0, 0, 0, PORT6);
+  viewer->addText("TRUNK", 10, 10, "PORT6", PORT6);
 
-  viewer->addPointCloud<pcl::PointXYZ>(Map3D, "Map3d", PORT1);
-  viewer->addPointCloud<pcl::PointXYZRGB>(Map3DDense, "Map3dDense", PORT2);
-  viewer->addPointCloud<pcl::PointXYZRGB>(cloud_3dMap_scaled, "Map3dScaled", PORT3);
-  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> trunk_color(trunk_segmented, 230, 20, 20);
+  viewer->addPointCloud(Map3D, "Map3d", PORT1);
+  viewer->addPointCloud(Map3DDense, "Map3dDense", PORT2);
+  viewer->addPointCloud(cloud_3dMap_scaled, "Map3dScaled", PORT3);
+
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> trunk_color(trunk_segmented, 0,255, 0);
   viewer->addPointCloud(trunk_segmented,trunk_color, "Trunk", PORT6);
-  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> crown_color(trunk_segmented, 0, 255, 0);
-  viewer->addPointCloud(trunk_segmented,crown_color, "Crown", PORT5);
-  viewer->addPointCloud(trunk_segmented, "TreeSegmented", PORT4);
+  viewer->addLine(minDBH,maxDBH,255,0,0,"DBH",PORT6);
+
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> tree_color(tree_segmented, 255, 255, 0);
+  viewer->addPointCloud(tree_segmented,tree_color, "Crown", PORT5);
+
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> crown_color(trunk_segmented, 255, 0, 255);
+  viewer->addPointCloud(crown_segmented,crown_color,"TreeSegmented", PORT4);
 
   viewer->setPosition(0,0);
   viewer->addCoordinateSystem ();
@@ -149,8 +158,6 @@ int main(int argc, char **argv){
   while(!viewer->wasStopped ()) {
          viewer->spin();
   }
-
-
 
   return 0;
 }
