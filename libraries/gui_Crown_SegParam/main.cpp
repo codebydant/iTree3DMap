121a0b3eb13a7baa549f9bf0bd123051c4d50804
Daniel <35694200+danielTobon43@users.noreply.github.com>
2018-10-15 16:43:07 -0500

    Add files via upload

---
+++ libraries/gui_Crown_SegParam/main.cpp
@@ -0,0 +1,28 @@
+/*********************************
+           HEADERS
+**********************************/
+#include <QApplication>
+#include "include/Visualizer.h"
+
+/*********************************
+      FUNCION PRINCIPAL-MAIN
+**********************************/
+
+int main(int argc, char **argv ){
+
+  QApplication container(argc, argv);
+
+  if(argc<3 or argc > 3){
+      std::cout << "Usage: <crown parameters.txt> <output dir>" << std::endl;
+      return -1;
+  }
+
+  Visualizer window(argc,argv);
+  window.setWindowFlags(Qt::WindowTitleHint | Qt::WindowMinimizeButtonHint);
+  window.move(0,0);
+  window.show(); 
+  window.setFixedSize(302,263);
+
+  return container.exec();
+
+}//end main
